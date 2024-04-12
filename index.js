ee787b07fc846e4eb012a4aecec9871453cf984b
PraveenNendrambakam <160592930+PraveenNendrambakam@users.noreply.github.com>
2024-04-12 19:45:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var sidenav = document.querySelector(".side-navbar")
+
+function showNavbar()
+{
+    sidenav.style.dispaly = "block"
+   
+}
+
+function closeNavbar()
+{
+    sidenav.style.dispaly = "none"
+}
